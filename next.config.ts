c64aac3f7763d1d3b216e106e5b712c445b0ae8a
mfried95 <m.friedman1995@gmail.com>
2026-01-18 00:17:32 -0500

    Coverage card animations added

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
-    domains: ["images.pexels.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
-
-
-
 
 export default nextConfig;
